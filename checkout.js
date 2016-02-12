ef16267beecd95860c0b262a240cd478714981ed
Erik Skurka <erik.skurka@powerreviews.com>
2016-02-12 15:14:48 -0600

    Create checkout.js
    Script to deploy on thank you page of order checkout flow to pass data back to the PowerReviews system.

---
+++ checkout.js
@@ -0,0 +1,27 @@
+<!­­Conversion Beacon Code Starts­­>
+<script type="text/javascript"
+src="//static.powerreviews.com/t/v1/tracker.js"></script>
+<script type="text/javascript">
+(function(){try{
+var tracker = POWERREVIEWS.tracker.createTracker({
+merchantGroupId: "<yourMerchantGroupId>​"});
+tracker.trackPageview("c", {
+merchantId: "<yourMerchantId>​",
+locale: "en_US",
+merchantUserId: "<yourMerchantUserId>​",
+marketingOptIn: <true­or­false>​,
+userEmail: "<yourUserEmail>"​,
+userFirstName: "<yourUserFirstName>"​,
+userLastName: "<yourUserLastName>​",
+orderId: "<yourOrderId>​",
+orderSubtotal: "<yourOrderSubtotal>​",
+orderNumberOfItems: "<yourNumberOfItems>​",
+orderItems: [ // array of item arrays
+["<pageId>​", "<pageIdVariant>​",“​productName​”​,​"<qty>​",
+"<unitPrice>​"] // if more items, add a comma
+// add additional item(s) arrays
+]
+});
+}catch(e){window.console && window.console.log(e)}}());
+</script>
+<!­­Conversion Beacon Code Ends­­>
